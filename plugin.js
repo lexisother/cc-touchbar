f6c0560ef3dafa40e086c0b33334d18e43189f75
Alyxia Sother <alyxia@riseup.net>
2023-09-28 13:35:45 +0200

    [mod] Set the button label when switching player configs

--- plugin.js
+++ plugin.js
@@ -1,8 +1,18 @@
 export default class CCTouchbar {
   constructor(mod) {
-    const addon = require(`${mod.baseDirectory}build/Release/addon.node`);
+    const addon = (this.addon = require(`${mod.baseDirectory}build/Release/addon.node`));
 
     console.log(addon.hello());
     addon.setTouchbar();
+  }
+
+  prestart() {
+    const addon = this.addon;
+    ig.ENTITY.Player.inject({
+      initModel() {
+        this.parent();
+        addon.setButtonLabel(sc.model.player.config.name);
+      },
+    });
   }
 }
